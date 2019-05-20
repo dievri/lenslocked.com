e1c9cd1790142bf5d544a2654fc8522bb080a57b
Oleksandr Dievri <o.dievri@outlook.com>
2019-05-20 21:17:51 +0300

    change gitignore

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	r.Header.Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
