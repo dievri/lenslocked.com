5a91f30d553eca4d84cd32d2a49a61d82ff9fed4
Oleksandr Dievri <o.dievri@outlook.com>
2019-05-21 12:44:59 +0300

    Added a 404 page

--- main.go
+++ main.go
@@ -13,13 +13,18 @@ func main() {
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("SomeOne visited our site")
 	r.Header.Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<html><body>")
+
 
 	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<html><body>")
 		fmt.Fprint(w, "<h1>Welcome to my sssuper awesome site!</h1>")
 	} else if r.URL.Path =="/contact" {
+		fmt.Fprint(w, "<html><body>")
 		fmt.Fprint(w, "To get in touch, please send me email to <a href=\"mailto:o.dievri@outlook.com\">support</a><br>")
-
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<html><body>")
+		fmt.Fprint(w, "<h1>We cannot find a page 404</h1>")
 	}
 	fmt.Fprint(w, r.URL.Path)
 	fmt.Fprint(w, "</body></html>")
