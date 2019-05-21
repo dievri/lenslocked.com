20bf4891dbec22d96786e667dd4c8f80f37480ae
Oleksandr Dievri <o.dievri@outlook.com>
2019-05-21 11:33:47 +0300

    add routing on route home and contact

--- main.go
+++ main.go
@@ -13,7 +13,14 @@ func main() {
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("SomeOne visited our site")
 	r.Header.Set("Content-Type", "text/html")
-	//r.Header.Set("Custom-Head", "Ferrari Superfast")
-	//fmt.Fprint(w, "<h1>Welcome to my sssuper awesome site!</h1>")
-	fmt.Fprint(w, "<html><body>To get in touch, please send me email to <a href=\"mailto:o.dievri@outlook.com\">support</a></body></html>")
+	fmt.Fprint(w, "<html><body>")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my sssuper awesome site!</h1>")
+	} else if r.URL.Path =="/contact" {
+		fmt.Fprint(w, "To get in touch, please send me email to <a href=\"mailto:o.dievri@outlook.com\">support</a><br>")
+
+	}
+	fmt.Fprint(w, r.URL.Path)
+	fmt.Fprint(w, "</body></html>")
 }
