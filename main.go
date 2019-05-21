8b0da136e5b4d8adaee62f680c91a7c70e82ba22
Oleksandr Dievri <o.dievri@outlook.com>
2019-05-21 11:19:31 +0300

    Add custom header

--- main.go
+++ main.go
@@ -13,5 +13,6 @@ func main() {
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("SomeOne visited our site")
 	r.Header.Set("Content-Type", "text/html")
+	r.Header.Set("Custom-Head", "Ferrari Superfast")
 	fmt.Fprint(w, "<h1>Welcome to my sssuper awesome site!</h1>")
 }
