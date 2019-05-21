7ad0600e14fd1144bac335a30ba6220c2e7090a1
Oleksandr Dievri <o.dievri@outlook.com>
2019-05-21 11:25:19 +0300

    adding html rendering

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("SomeOne visited our site")
 	r.Header.Set("Content-Type", "text/html")
-	r.Header.Set("Custom-Head", "Ferrari Superfast")
-	fmt.Fprint(w, "<h1>Welcome to my sssuper awesome site!</h1>")
+	//r.Header.Set("Custom-Head", "Ferrari Superfast")
+	//fmt.Fprint(w, "<h1>Welcome to my sssuper awesome site!</h1>")
+	fmt.Fprint(w, "<html><body>To get in touch, please send me email to <a href=\"mailto:o.dievri@outlook.com\">support</a></body></html>")
 }
