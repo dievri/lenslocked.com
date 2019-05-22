84ce9219c5faf3cc6b0a92b162ac30ad166df899
Oleksandr Dievri <o.dievri@outlook.com>
2019-05-22 09:03:39 +0300

    Added an FAQ page.

--- main.go
+++ main.go
@@ -15,10 +15,22 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w,"To get in touch, please send me email to <a href=\"mailto:o.dievri@outlook.com\">support</a>")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+	fmt.Fprint(w, "The page you looking for is not found")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "NEVER GIVE UP!")
+}
+
 func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8080", r)
 }
